6c6aab37ce4ba7a4d673b4c7337379de54773900
tgkd <p.y.trofimov@gmail.com>
2023-09-28 08:03:08 +0400

    worker init

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,101 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono/quick';
+import { cache } from 'hono/cache';
+import { sha256 } from 'hono/utils/crypto';
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+interface EmailRequest {
+  from: string;
+  to: string;
+  subject?: string;
+  html?: string;
 }
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+interface RequestData {
+  receiver: string;
+  payload: string[];
+}
+
+type Env = {
+  API_HOST: string;
+  EMAIL_API_KEY: string;
+  SENDER_EMAIL: string;
+  IMG_BUCKET: R2Bucket;
 };
+
+const app = new Hono<{ Bindings: Env }>();
+
+app.put('/upload', async (c) => {
+  const data = await c.req.json<RequestData>();
+  const { payload, receiver } = data;
+
+  if (!receiver || !Array.isArray(payload) || payload.length === 0) {
+    return c.notFound();
+  }
+
+  for await (const i of payload) {
+    const body = Uint8Array.from(atob(i.replace(/^data[^,]+,/, '')), (c) =>
+      c.charCodeAt(0)
+    );
+    const key = (await sha256(body)) + '.' + 'png';
+
+    await c.env.IMG_BUCKET.put(key, body, {
+      httpMetadata: { contentType: 'image/png' },
+    });
+  }
+
+  const body: EmailRequest = {
+    from: c.env.SENDER_EMAIL,
+    to: receiver,
+    subject: 'hello world',
+    html: '<strong>it works!</strong>',
+  };
+
+  const eRes = await fetch(c.env.API_HOST, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${c.env.EMAIL_API_KEY}`,
+    },
+    body: JSON.stringify(body),
+  });
+
+  const res = await gatherResponse(eRes);
+
+  return c.text(res);
+});
+
+async function gatherResponse(response: Response) {
+  const { headers } = response;
+  const contentType = headers.get('content-type') || '';
+
+  if (contentType.includes('application/json')) {
+    return JSON.stringify(await response.json());
+  }
+  return response.text();
+}
+
+/*
+
+const maxAge = 60 * 60 * 24 * 30;
+
+app.get(
+  '*',
+  cache({
+    cacheName: 'frosty-king-4811',
+  })
+);
+
+app.get('/:key', async (c) => {
+  const key = c.req.param('key');
+
+  const object = await c.env.IMG_BUCKET.get(key);
+  if (!object) return c.notFound();
+  const data = await object.arrayBuffer();
+  const contentType = object.httpMetadata?.contentType ?? '';
+
+  return c.body(data, 200, {
+    'Cache-Control': `public, max-age=${maxAge}`,
+    'Content-Type': contentType,
+  });
+});
+ */
+export default app;
